bdadf8e0f1da36affcbd6e1a0a55f2ae46bded85
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2024-02-05 07:42:48 +0300

    Создает функцию isNumber

--- js/function.js
+++ js/function.js
@@ -8,5 +8,23 @@ const isPalindrom = function (string) {
   return 'Палиндром';
 };
 
-
 console.log(isPalindrom('каюк'));
+
+const isNumber = function (string) {
+  let result = '';
+  for (let i = 0; i <= string.length; i++) {
+    const letter = parseInt(string.at(i), 10);
+    if ((!isNaN(letter))) {
+      result += letter;
+    }
+  }
+  if (result === '') {
+    return NaN;
+  }
+  return parseInt(result, 10);
+};
+
+isNumber('а я томат');
+
+console.log(isNumber('а я томат'));
+
