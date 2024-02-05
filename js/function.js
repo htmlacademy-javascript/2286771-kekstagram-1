76dd00a4555cee8cdc1512ca829d92762a26abc4
Inna Ladyzhenskaia <pinguinil2000@gmail.com>
2024-02-05 18:17:53 +0300

    Создает функцию с тремя параметрами

--- js/function.js
+++ js/function.js
@@ -28,3 +28,27 @@ isNumber('а я томат');
 
 console.log(isNumber('а я томат'));
 
+
+const createNewString = function (originalString, minLength, additionalString) {
+
+  let newString = originalString;
+  if (originalString.length >= minLength) {
+    return newString;
+  }
+  const additionalStringLength = minLength - originalString.length;
+  newString = additionalString.slice(0, additionalStringLength) + newString;
+
+  if (newString.length <= minLength) {
+    const newStringLength = minLength - newString.length;
+    for (let i = newStringLength; i >= 1; i--) {
+      newString = additionalString.slice(0, i) + newString;
+    }
+    console.log(newString);
+  }
+
+};
+
+createNewString('q', 4, 'werty');
+
+console.log(createNewString('q', 4, 'werty'));
+
